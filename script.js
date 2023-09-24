aee8d39f0003161f51d3c02960c51457699b9ccc
eva-olivia <145458476+eva-olivia@users.noreply.github.com>
2023-09-24 21:04:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+"user strict"
+
+let prenom="john";
+
+let santeMental=10;
+
+let feurouge=30;
+let musiqueAleatoire;
+let musique=["annissa-wejdene" , "aya-oula" , "florence and the machine-to wreck" , "adele-set fire to rain" , "beyonce-xo"];
+
+function random() {
+
+    let musiqueAleatoire = Math.floor(Math.random() * 5);
+
+
+      return musiqueAleatoire;
+}
+while (santeMentale > 0 && nbEssais < 0) {
+    musiqueAleatoire=musique[random()];
+    console.log("jonh a ecouter de la musique"+" musiqueAleatoire "+"santeMental"+"point");
+
+    if (musiqueAleatoire==musique[0]){;
+        santeMental =-1;
+    }
+    feurouge--;
+}
+   if(santeMental<=0){
+console.log("john explose sante mentale 0");
+   }else{
+    console.log("jonh rentre chez lui"+"santeMentale"+"point mentale");
+   }
+   console.log("nbr de feux rouge restant:"+"nbrFeurouge");
+   
+
